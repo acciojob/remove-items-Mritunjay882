03ba1e3c41011c964631d683e89747e017a521d8
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 12:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// script.js
+function removeColor() {
+  var select = document.getElementById("colorSelect");
+  var selectedOption = select.selectedIndex;
+  select.remove(selectedOption);
+  alert("Color removed!");
+}
